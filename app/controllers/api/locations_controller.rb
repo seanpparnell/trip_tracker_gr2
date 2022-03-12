f4824629b6564da477eb7bafb4a7df3920120d46
Axel-Segovia19 <14segovia@gmail.com>
2022-03-12 11:03:04 -0700

    added location and addy controller

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,2 +1,48 @@
 class Api::LocationsController < ApplicationController
+  before_action :set_trip 
+  before_action :set_location, only: [:show, :update, :destroy]
+
+  def index
+    render json: @set_trip.locations
+  end
+
+  def show
+    render json: @location
+  end
+
+  def create
+    @location = @trip.locations.new(location_params)
+    if @location.save
+      render json: @location
+    else
+      render json: { errors: @location.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @location.update(location_params)
+      render json: @location
+    else
+      render json: { errors: @location.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @location.destroy
+    render json: { message: 'Location Deleted'}
+  end
+
+  private 
+    def set_trip
+      @trip = Trip.find(params[:trip_id])
+    end
+
+    def set_location
+      @location = @trip.locations.find(params[:id])
+    end
+
+    def locations_params
+      params.require(:location).permit(:name, :where)
+    end
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :location
+
+  validates :street, :city, :state, :zip, presence: true
 end
