a83a4280a108ad14db9d34647339bcc2b16a0730
Preston Brinkerhoff <prestonbrinkerhoff1@gmail.com>
2022-03-12 11:07:13 -0700

    controllers

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,2 +1,54 @@
 class Api::TripsController < ApplicationController
+  before_action :set_user
+  before_action :set_trip, only: [:show, :update, :destroy]
+
+  def index
+    
+    render json: @user.trips
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def create
+  
+    @trip = @user.trips.new(trip_params)
+    if @trip.save
+      render json: @trip
+    else
+      render json: { errors: @trip.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: { errors: @trip.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    render json: { message: 'Trip Deleted'}
+  end
+
+  private 
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_trip
+      
+      @trip = user.trips.find(params[:id])
+    end
+
+    def trips_params
+      params.require(:trip).permit(:name, :duration)
+    end
+
+
+
+
 end
